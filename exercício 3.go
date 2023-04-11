4a16b8e5c3bf6cbfcb92b3fdf2d11af950ce6dce
eduardomlg <126572421+eduardomlg@users.noreply.github.com>
2023-04-11 09:18:30 -0300

    Update exercício 3.go

--- exercício 3.go
+++ exercício 3.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var numeros = [4]float{1.2, 2, 3, 4}
+	var numeros = [4]int{1, 2, 3, 4}
 	produto := 1
 	for i := 0; i < len(numeros); i++ {
 		produto *= numeros[i]
